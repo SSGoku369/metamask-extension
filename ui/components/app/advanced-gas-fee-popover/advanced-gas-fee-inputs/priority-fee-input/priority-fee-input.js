1f0998f1e5a43d2531d9408a2697a61a0213da9a
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2022-01-06 21:00:38 -0500

    removing the multiplier part of max base fee and updating the transaction controller

--- ui/components/app/advanced-gas-fee-popover/advanced-gas-fee-inputs/priority-fee-input/priority-fee-input.js
+++ ui/components/app/advanced-gas-fee-popover/advanced-gas-fee-inputs/priority-fee-input/priority-fee-input.js
@@ -109,7 +109,7 @@ const PriorityFeeInput = () => {
         error={priorityFeeError ? t(priorityFeeError) : ''}
         onChange={updatePriorityFee}
         titleText={t('priorityFeeProperCase')}
-        titleUnit="(GWEI)"
+        titleUnit={t('gweiInParanthesis')}
         tooltipText={t('advancedPriorityFeeToolTip')}
         value={priorityFee}
         detailText={`≈ ${priorityFeeInFiat}`}
